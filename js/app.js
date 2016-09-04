3f6052e3b4add205ba6e63a474218ec2c6cfe369
Rosalino Cabral <rosalino.cabral@gmail.com>
2016-09-03 22:07:23 -0400

    Tarea5

---
+++ js/app.js
@@ -0,0 +1,29 @@
+//funcion anonima auto aejecutable
+(funcion(){
+console.log("Tarea5");
+
+
+}());
+//variable del tipo array
+var array = [];
+
+//variable de tipo lera
+var letra ='Tarea5';
+
+//variable de tipo numerica
+var numerica = 7;
+
+//array con su primer elemento
+array.push('Tarea5');
+
+//array con el segundo elemto
+array.push('elemto2');
+
+//UNA VARIABLE DEL TIPO OBJETO
+var objeto = {};
+//atributo del objeto
+objeto.descripcion ='Tarea5';
+//atributo del objeto
+objeto.cantidad = 40;
+
+console.log(objeto);
